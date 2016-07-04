06d5d1f8bb112776cd00a30eb7e74edbdd0399a0
leo424y <leo424y@gmail.com>
2016-07-04 20:40:11 +0800

    Step 2: 實作看板的新增 new / create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,24 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
