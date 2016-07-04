6de063896bd0f9efb34897dc30e8f8cec327be9a
leo424y <leo424y@gmail.com>
2016-07-04 21:21:06 +0800

    Step 5 : 在 group 裡面可以發表文章 new/create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+ def new
+   @group = Group.find(params[:group_id])
+   @post = @group.posts.new
+ end
+
+ def create
+   @group = Group.find(params[:group_id])
+   @post = @group.posts.build(post_params)
+
+   if @post.save
+     redirect_to group_path(@group), notice: "新增挑戰行動成功！"
+   else
+     render :new
+   end
+ end
+
+ private
+
+ def post_params
+   params.require(:post).permit(:content)
+ end
 end
