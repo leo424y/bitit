d610c4763a3d1e5d1e9c6ea14d84dd65b7e06c37
leo424y <leo424y@gmail.com>
2016-07-04 20:12:58 +0800

    gem "simple_form"  bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :groups
-  root 'welcome#index'
+  root 'groups#index'
+  #root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
