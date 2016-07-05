3928af90b55e260da9524e27bfe4c96f5a68c505
leo424y <leo424y@gmail.com>
2016-07-05 20:25:14 +0800

    建立 討論版成員 ( group_user ) 設定
    rails g model group_user group_id:integer user_id:integer
    新增一個叫 group_user 資料表, 裡面的欄位是 group_id 跟 user_id
    
    rake db:migrate
    執行資料庫異動設定
    
    打開 app/models/user.rb
    
    app/models/user.rb
    ...
    ...
      has_many :groups
      has_many :posts
    
    + has_many :group_users
    + has_many :participated_groups, through: :group_users, source: :group
    end
    打開 app/models/group.rb
    
    app/models/group.rb
    class Group < ActiveRecord::Base
      validates :title, presence: true
    
      has_many :posts
    + has_many :group_users
    + has_many :members, through: :group_users, source: :user
    ..
    ..
    打開 app/models/group_user.rb
    
    原本
    
    app/models/group_user.rb
    class GroupUser < ActiveRecord::Base
    + belongs_to :user
    + belongs_to :group
    end

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :groups
   has_many :posts
 
+  has_many :group_users
+  has_many :participated_groups, through: :group_users, source: :group
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
