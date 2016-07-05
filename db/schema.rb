1a8706c8a0771e511d296972d9429c3d8daaf0e6
leo424y <leo424y@gmail.com>
2016-07-05 17:56:23 +0800

    group 資料表新增 user_id 欄位
    http://rails.ruby.tw/active_record_migrations.html

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705053615) do
+ActiveRecord::Schema.define(version: 20160705095349) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
