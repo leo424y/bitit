dd289290dcd3809c36c0c8724caded2eb94e1ec0
leo424y <leo424y@gmail.com>
2016-07-06 18:25:40 +0800

    撰寫 seed.rb 檔
    我們在開發專案的時候，常會需要弄些假資料來填充版面
    或是說當資料庫炸掉的時候，會很想一股腦把 資料庫整個 reset 掉
    並希望 reset 完還能把一些資料放進去，像是 user, groups, posts，不用一個個手動輸入
    
    我們來做一個 seed 檔，需求:
    
    建立一個 user 帳號 => example@gmail.com , 密碼 12345678
    並用此帳號建立 20 個 groups
    每個 group 各有 30 個 posts
    打開 db/seeds.rb
    
    db/seeds.rb
    -(空的)
    +puts "Hello World!"
    
    rake db:seed
    執行 db:seed
    
    螢幕快照 2014-07-24 下午2.21.57.png
    
    db/seeds.rb
    puts "Hello World!"
    
    +create_account = User.create([email: 'example@gmail.com', password:
    '12345678', password_confirmation: '12345678', name: '測試用帳號'])
    
    rake db:seed
    
    螢幕快照 2014-07-24 下午2.35.38.png
    
    db/seeds.rb
    puts 'Hello World!'
    + puts "這個種子檔會自動建立一個帳號, 並且創建 20 個 groups, 每個 group 各 30 個 posts"
    
    create_account = User.create([email: 'example@gmail.com', password:
    '12345678', password_confirmation: '12345678', name: '測試用帳號'])
    
    + create_groups = for i in 1..20 do
    +                   Group.create!([title: "Group no.#{i}", description:
    "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
    +                   GroupUser.create(group_id: i, user_id: 1)
    +                   for k in 1..30 do
    +                     Post.create!([group_id: "#{i}",content:
    "這是用種子建立的第 #{k} 個留言", user_id: "1"])
    +                   end
    +                 end
    
    rake db:reset
    rake db:reset 是內建的 task ，會執行一整套流程
    包含 db:drop( 資料庫移除 ) => db:create( 資料庫建立 )
    => db:schema:load ( 資料庫欄位建立 ) => db:seed ( 執行資料庫種子 )
    
    螢幕快照 2014-07-24 下午2.42.10.png
    
    重新啟動 rails s

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Hello World!"
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用帳號'])
+
+create_groups = for i in 1..20 do
+                  Group.create!([title: "Group no.#{i}", description: "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
+                  GroupUser.create(group_id: i, user_id: 1)
+                  for k in 1..30 do
+                    Post.create!([group_id: "#{i}",content: "這是用種子建立的第 #{k} 個留言", user_id: "1"])
+                  end
+                end
