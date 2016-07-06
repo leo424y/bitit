5431808568ce947a994ee6bb0fbd1ca7e7a08f8b
leo424y <leo424y@gmail.com>
2016-07-06 13:19:24 +0800

    使用者可以在後台看到自己發表的 post
    Bug fix: 將沒有 group_id 的文章刪除掉
    在做下面開發前，先確認之前有沒有因為尚未實作 model 關聯前所建立的 post 資料
    導致該 post 沒有 group_id, 會在下面開發時發生錯誤
    
    解決方法:
    
    rails c 到 rails console 模式
    
    Post.where(group_id: nil) 確認有沒有 group_id 是空的 post
    
    Post.where(group_id: nil).destroy_all 將那些 post 刪除掉
    
    螢幕快照 2016-02-22 下午4.16.32.png
    
    打開 app/controllers/account/posts_controller.rb
    
    app/controllers/account/posts_controller.rb
    class Account::PostsController < ApplicationController
    
    + before_action :authenticate_user!
    +
    + def index
    +   @posts = current_user.posts
    + end
    end
    打開 app/views/account/posts/index.html.erb
    
    app/views/account/posts/index.html.erb
    <div class="col-md-12">
    
      <h2 class="text-center"> 我發表過的文章 </h2>
    
      <table class="table">
        <thead>
          <tr>
            <th> Content </th>
            <th> Group Name </th>
            <th> Last Update </th>
            <th colspan="2"></th>
          </tr>
        </thead>
    
        <tbody>
          <% @posts.each do |post| %>
            <tr>
              <td> <%= post.content %> </td>
              <td> <%= post.group.title %> </td>
              <td> <%= post.updated_at %> </td>
              <td> <%= link_to('Edit', edit_group_post_path(post.group,
    post), class: "btn btn-info btn-xs") %></td>
              <td> <%= link_to('Delete', group_post_path(post.group, post),
    method: :delete, data: { confirm: "Are you sure?" }, class: "btn
    btn-danger btn-xs") %></td>
            </tr>
          <% end %>
        </tbody>
      </table>
    </div>

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :group_users
   has_many :members, through: :group_users, source: :user
 
