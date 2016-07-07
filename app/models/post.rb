a07d245a2904e07400447abd9e922d63e5b7bb07
leo424y <leo424y@gmail.com>
2016-07-07 09:09:07 +0800

    validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
   scope :recent, -> { order("updated_at DESC")}
 
-  validates :content, presence: true
-  validates_length_of :content, :maximum => 20
+  validates :content, presence: true, length: { maximum: 25 }
   belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
