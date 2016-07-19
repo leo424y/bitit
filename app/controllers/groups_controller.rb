efd385381e57cb05fec930d6dded2d4dcf18c5fd
leo424y <leo424y@gmail.com>
2016-07-19 13:57:46 +0800

    group.font :)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -76,6 +76,6 @@ class GroupsController < ApplicationController
   end
 
   def group_params
-    params.require(:group).permit(:title, :description)
+    params.require(:group).permit(:title, :description, :font)
   end
 end
