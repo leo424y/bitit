a2828cd73a5e8c9dd511d07fa2f0e5285fc07f4e
leo424y <leo424y@gmail.com>
2016-07-22 00:00:20 +0800

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
 
   protect_from_forgery with: :exception
+
+
+
   protected
 
   def configure_permitted_parameters
