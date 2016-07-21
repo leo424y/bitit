4edd3d7141e557929ec25879a2226ee13566ef9c
leo424y <leo424y@gmail.com>
2016-07-21 09:32:45 +0800

    721

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ puts "Hello World!"
 create_account = User.create([email: 'a@a.a', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', name: 'A_A'])
 
 create_groups = for i in 1..20 do
-                  Group.create!([title: "Group no.#{i}", description: "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
+                  Group.create!([title: "字#{i}", description: "這是用種子建立的第 #{i} 個討論版", user_id: "1"])
                   GroupUser.create(group_id: i, user_id: 1)
                   for k in 1..30 do
                     Post.create!([group_id: "#{i}",content: "這是用種子建立的第 #{k} 個留言", user_id: "1"])
