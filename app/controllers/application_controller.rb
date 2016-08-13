9ce78a0c877f75c37702e5e5456f380982db3c2b
leo424y <leo424y@gmail.com>
2016-08-13 20:57:53 +0800

    SEO go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,6 @@ class ApplicationController < ActionController::Base
       description = "拼一個自己的字! 讓我們造字，不造次。在日子裡找屬於你的字，用它談談你的故事。"
       image       = options[:image] || "https://tools.wmflabs.org/idsgen/%E2%BF%B1%E2%BF%B1%E8%87%BC%E5%A4%A7%E5%BF%83.png?%E5%AD%97%E9%AB%94=%E5%AE%8B%E9%AB%94"
       current_url = request.url
-
       defaults = {
         site:        site_name,
         title:       title,
@@ -46,21 +45,12 @@ class ApplicationController < ActionController::Base
         description: description,
         canonical:   current_url,
         keywords:    %w[中文 國文 造字 合字 合文 倉頡 金萱 字型 字體 台灣 新年 春聯 無蝦米 招財進寶 合字文間 justfont],
-        og:          {url: current_url,　　# 設定 Facebook OG 預設資訊內容
-
+        og:          {url: current_url,
                       site_name: site_name,
                       title: title,
                       image: image,
                       description: description,
                       type: 'website'}
-      }　# 此為預設資訊內容
-
-      options.reverse_merge!(defaults)　
-  　　# 透過 options.reverse_merge! 讓其他 controller 中可作設定（若沒有額外設定就是預設）
-
-      set_meta_tags options
-      # 最後透過 set_meta_tags 設定 meta data 資訊
-      # 參考自 http://georgiowan.logdown.com/notes/441929/rails-url-seo-methods-via-meta-tags
-    end
-
+      }
+  end
 end
